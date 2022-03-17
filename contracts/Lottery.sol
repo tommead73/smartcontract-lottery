45819d7824b93baefd5c7551c3eec9cbe3a3eabd
Tom Maad <tom.mead2@gmail.com>
2022-03-17 14:16:32 +0000

    updates

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,7 +1,18 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.6.6;
+
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
 
 contract Lottery {
     address payable[] public players;
+    uint256 public usdEntryFee;
+    AggregatorV3Interface internal ethUsdPriceFeed;
+
+    constructor(address _priceFeedAddress) public {
+        usdEntryFee = 50 * (10**18);
+        ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
+    }
 
     function enter() public payable {
         // $50 minimum
