41d30d577250ed6dd3a263efda70a0dc0db98c0d
Tom Maad <tom.mead2@gmail.com>
2022-03-17 07:19:19 +0000

    move to desktop

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,1 +1,18 @@
 pragma solidity ^0.6.6;
+
+contract Lottery {
+    address payable[] public players;
+
+    function enter() public payable {
+        // $50 minimum
+        players.push(msg.sender);
+    }
+
+    function getEntranceFee() public view returns (uint256) {
+        // ?
+    }
+
+    function startLottery() public {}
+
+    function endLottery() public {}
+}
