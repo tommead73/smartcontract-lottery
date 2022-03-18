06d7a876089d2b59e31cdcd68ceb7767e5341fd4
Tom Maad <tom.mead2@gmail.com>
2022-03-18 07:11:06 +0000

    morning updates

---
+++ scripts/deploy_Lottery.py
@@ -0,0 +1,9 @@
+from scripts.helpful_scripts import get_account
+from brownie import Lottery
+
+def deploy_lottery():
+    account = get_account()
+    lottery = Lottery.deploy()
+
+def main():
+    deploy_lottery()
